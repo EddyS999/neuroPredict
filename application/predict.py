ab15092b4db165965313f11eb2c4e9d839b7e1bd
eddys999 <eddy20003@gmail.com>
2023-05-25 02:50:46 +0200

    ajout du modèle de regression

--- application/predict.py
+++ application/predict.py
@@ -23,3 +23,20 @@ def get_prediction(sexe, age, poids, taille, salivation, cutting, turning, alsfr
         return 0
     # on retourne une liste contenant : [chance mort, chance survie, prediction]
     return result
+
+
+def get_prediction_reg(sexe, age, poids, taille, salivation, cutting, turning, alsfrs, symptom_duration, pulse, systolic_blood_pressure):
+    result = []
+    if os.path.getsize('application/model/als_model_reg.pkl') > 0:
+        with open('application/model/als_model_reg.pkl', 'rb') as file:
+            model = pickle.load(file)
+        with open('application/model/als_model_reg.pkl', 'wb') as file:
+            pickle.dump(model, file)
+
+        patient = np.array([sexe, age, poids, taille, salivation, cutting,
+                           turning, alsfrs, symptom_duration, pulse, systolic_blood_pressure])
+        prediction = model.predict(patient.reshape(1, -1))[0]
+        return prediction.tolist()
+
+    else:
+        return 0
